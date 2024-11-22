ac8dd96b1de43a094f904da65d09a960c6378558
Teruozyy <00001084784439sp@al.educacao.sp.gov.br>
2024-11-22 16:04:57 -0300

    teruo
    teruo

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'Geografia',
+    'Qual é a maior cidade do Brasil em população?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um triângulo?',
+    'Área = (base x altura) / 2'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual órgão do corpo humano é responsável pela produção de insulina?',
+    'Pâncreas'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a unidade de medida da força no Sistema Internacional?',
+    'Newton (N)'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma reação de neutralização?',
+    'Reação entre um ácido e uma base que resulta em sal e água'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Independência do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual é a obra mais famosa de William Shakespeare?',
+    'Hamlet'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o nome da nossa galáxia?',
+    'Via Láctea'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior deserto do mundo?',
+    'Deserto da Antártida'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Número inteiro maior que 1 que só pode ser dividido por 1 e por ele mesmo'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é a célula?',
+    'A menor unidade estrutural e funcional dos seres vivos'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a fórmula da Lei de Ohm?',
+    'V = R x I'
+)
+
+criaCartao(
+    'Química',
+    'Quem é o cientista que propôs a tabela periódica?',
+    'Dmitri Mendeliev'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a principal causa da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "O Senhor dos Anéis"?',
+    'J.R.R. Tolkien'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual planeta é conhecido como o "Planeta Vermelho"?',
+    'Marte'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular o perímetro de um círculo?',
+    'Perímetro = 2πr'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os dois principais tipos de células?',
+    'Células procariontes e eucariontes'
+)
+
+criaCartao(
+    'Física',
+    'O que é a energia cinética?',
+    'Energia associada ao movimento de um corpo'
+)
